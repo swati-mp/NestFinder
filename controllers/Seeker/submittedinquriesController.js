bdc2b66669ab0635a43933e25e47a0dd1fb1e9f6
swati-mp <8swatipatil21@gmsil.com>
2025-01-09 20:46:45 +0530

    Added submit Inquiry Interface

--- controllers/Seeker/submittedinquriesController.js
+++ controllers/Seeker/submittedinquriesController.js
@@ -1,5 +1,7 @@
 const getSubmittedInquries = (req,res) => {
-    res.render("submittedinquires")
+    res.render("submittedinquires",{
+        email:req.cookies.email,
+    })
 }
 
 module.exports = {
