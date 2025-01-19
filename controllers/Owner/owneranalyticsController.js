267b9ecc7823f3e858cf7c9f3afcc14da84901be
SniperNikhil <nikhilmahendrakar02@gmail.com>
2025-01-19 20:23:40 +0530

    Added Owner Analytics functionality in Owner Module

--- controllers/Owner/owneranalyticsController.js
+++ controllers/Owner/owneranalyticsController.js
@@ -1,11 +1,22 @@
+const { OwnerNotification } = require("../../model/Owner/ownernotificationSchema")
 const { owners } = require("../../model/Owner/ownerSchema")
+const { Property } = require("../../model/Owner/PropertySchema")
 
 const getOwnerAnalytics=async(req,res)=>{
     const ownerprofileimage = await owners.findOne({ email: req.cookies.email }).select("profilepicture")
 
+    const onwernotificationcount = await OwnerNotification.find({ ownerEmail: req.cookies.email }).countDocuments()
+    const totalproperties = await Property.find({ ownerEmail: req.cookies.email }).countDocuments()
+    const livepropertiescount = await Property.find({ ownerEmail: req.cookies.email, status: "Available",approved:true }).countDocuments()
+    const pendingpropertiescount = await Property.find({ ownerEmail: req.cookies.email,approved:false }).countDocuments()
+
     res.render("owneranalytics",{
         ownerprofileimage:ownerprofileimage.profilepicture,
-        email:req.cookies.email
+        email:req.cookies.email,
+        onwernotificationcount,
+        totalproperties,
+        livepropertiescount,
+        pendingpropertiescount
     })
 }
 module.exports={
