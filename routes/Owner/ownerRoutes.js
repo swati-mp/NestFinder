07ab14c5f000276f6aca74af644efc783607933e
SniperNikhil <nikhilmahendrakar02@gmail.com>
2025-01-19 20:58:38 +0530

    Added auth routes for all routes

--- routes/Owner/ownerRoutes.js
+++ routes/Owner/ownerRoutes.js
@@ -17,30 +17,30 @@ router.post("/searchproperties",ownerAuth,postSearchProperties)
 
 
 const {getOwnerProfile,postOwnerProfile,upload1}=require("../../controllers/Owner/ownerprofileController")
-router.get("/ownerprofile",getOwnerProfile)
-router.post("/ownerprofile",upload1.single('profilepicture'),postOwnerProfile)
+router.get("/ownerprofile",ownerAuth,getOwnerProfile)
+router.post("/ownerprofile",ownerAuth,upload1.single('profilepicture'),postOwnerProfile)
 
 const {getAddProperty,postAddProperties,uploadMultiple}=require("../../controllers/Owner/addpropertiesController")
-router.get("/owneraddproperties",getAddProperty)
-router.post("/owneraddproperties",uploadMultiple,postAddProperties)
+router.get("/owneraddproperties",ownerAuth,getAddProperty)
+router.post("/owneraddproperties",ownerAuth,uploadMultiple,postAddProperties)
 
 const {getMyProperty,getViewdetails,getUpdatedetails,postUpdateProperties,uploadMultiple1,deleteproperty}=require("../../controllers/Owner/mypropertiesController")
-router.get("/ownermyproperties",getMyProperty)
-router.get("/viewdetails/:id",getViewdetails)
-router.get("/updatedetails/:id",getUpdatedetails)
-router.post("/ownerupdateproperties", uploadMultiple1, postUpdateProperties);
-router.get("/deleteproperty/:id",deleteproperty)
+router.get("/ownermyproperties",ownerAuth,getMyProperty)
+router.get("/viewdetails/:id",ownerAuth,getViewdetails)
+router.get("/updatedetails/:id",ownerAuth,getUpdatedetails)
+router.post("/ownerupdateproperties",ownerAuth, uploadMultiple1, postUpdateProperties);
+router.get("/deleteproperty/:id",ownerAuth,deleteproperty)
 
 const {getOwnerAnalytics}=require("../../controllers/Owner/owneranalyticsController")
-router.get("/owneranalytics",getOwnerAnalytics)
+router.get("/owneranalytics",ownerAuth,getOwnerAnalytics)
 
 const {getOwnerAddComplaints,postOwnercomplaints}=require("../../controllers/Owner/owneraddcomplaintsController")
-router.get("/owneraddcomplaints",getOwnerAddComplaints)
-router.post("/addcomplaints",postOwnercomplaints)
+router.get("/owneraddcomplaints",ownerAuth,getOwnerAddComplaints)
+router.post("/addcomplaints",ownerAuth,postOwnercomplaints)
 
 
 const {getOwnerNotification,postOwnerNotification} = require("../../controllers/Owner/ownernotificationController")
-router.get("/ownernotification",getOwnerNotification)
-router.post("/readownernotification",postOwnerNotification)
+router.get("/ownernotification",ownerAuth,getOwnerNotification)
+router.post("/readownernotification",ownerAuth,postOwnerNotification)
 
 module.exports = router
--- routes/Seeker/seekerRoutes.js
+++ routes/Seeker/seekerRoutes.js
@@ -17,18 +17,18 @@ router.get("/viewdetails/:id",seekerAuth,getViewdetails)
 router.post("/searchproperties",seekerAuth,postSearchProperties)
 
 const {getSeekerProfile,postSeekerProfile,upload1} = require("../../controllers/Seeker/seekerprofileController")
-router.get("/seekerprofile",getSeekerProfile)
-router.post("/seekerprofile",upload1.single('profilepicture'),postSeekerProfile)
+router.get("/seekerprofile",seekerAuth,getSeekerProfile)
+router.post("/seekerprofile",seekerAuth,upload1.single('profilepicture'),postSeekerProfile)
 
 const {getSeekerWishlist} = require("../../controllers/Seeker/seekerwishlistController")
-router.get("/seekerwishlist",getSeekerWishlist)
+router.get("/seekerwishlist",seekerAuth,getSeekerWishlist)
 
 const {getSeekerComplaint,postSeekercomplaints} = require("../../controllers/Seeker/seekercomplaintController")
-router.get("/seekercomplaint",getSeekerComplaint)
-router.post("/addcomplaints",postSeekercomplaints)    
+router.get("/seekercomplaint",seekerAuth,getSeekerComplaint)
+router.post("/addcomplaints",seekerAuth,postSeekercomplaints)    
 
 const {getSeekerNotification,postSeekerNotification} = require("../../controllers/Seeker/seekernotificationController")
-router.get("/seekernotification",getSeekerNotification)
-router.post("/readseekernotification",postSeekerNotification)
+router.get("/seekernotification",seekerAuth,getSeekerNotification)
+router.post("/readseekernotification",seekerAuth,postSeekerNotification)
 
 module.exports = router
